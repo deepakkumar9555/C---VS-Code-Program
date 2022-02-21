5e9156f1a693032c00b64c11fa9c8aa4ca5d9162
DEEPAK KUMAR <deepakkumarjnv9555@gmail.com>
2022-02-21 14:40:08 +0530

    .

---
+++ FibonacciUsingRecursion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int fib(int n)
+{
+    if(n<=1)
+    {
+        return n;
+    }
+    int last=fib(n-1);
+    int slast=fib(n-2);
+    return last+slast;
+}
+int main()
+ {  
+     int n=4;
+     cout<<fib(n);
+    //cout<<"Hello World!";
+    return 0;
+}
